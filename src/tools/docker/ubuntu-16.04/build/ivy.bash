7dd4fecd184d7cde5fba49a5718e9cc950bda3fb
Xin Zhang <xzhang@pivotal.io>
2018-09-24 14:26:31 -0500

    FIXUP: ubuntu: ivy.bash: using /tmp to store the download files
    [#160659122]
    
    Authored-by: Xin Zhang <xzhang@pivotal.io>

--- src/tools/docker/ubuntu-16.04/build/ivy.bash
+++ src/tools/docker/ubuntu-16.04/build/ivy.bash
@@ -4,4 +4,8 @@ export ivyrepo_host=repo.pivotal.io
 export ivyrepo_user=build_readonly
 export ivyrepo_passwd="J3sFcOJ6VXbH3zqp"
 
+mkdir -p /tmp/
+cd /tmp/
+
 curl -u${ivyrepo_user}:${ivyrepo_passwd} -O "https://${ivyrepo_host}/gpdb-ext-release-local/emc/DDBoostSDK/3.3.0.4-550644/targzs/DDBoostSDK-rhel5_x86_64-3.3.0.4-550644.targz"
+tar xzvf DDBoostSDK-rhel5_x86_64-3.3.0.4-550644.targz
